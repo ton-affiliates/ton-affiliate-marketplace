12031b390d52782b901be3283eaf0df6f0fd7310
guyt-ds <58878443+guyt-ds@users.noreply.github.com>
2024-08-23 20:23:20 +0300

    Update NegativeTests.spec.ts

--- test/NegativeTests.spec.ts
+++ test/NegativeTests.spec.ts
@@ -15,7 +15,6 @@ describe('AffiliateMarketplace Negative Tests', () => {
     let bot;
     let advertiser;
     let publisher;
-    let affiliateContractAddress: string | null = null;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
@@ -39,171 +38,10 @@ describe('AffiliateMarketplace Negative Tests', () => {
             deploy: true,
             success: true,
         });
-
-        // Create an Affiliate contract
-        const cpc = toNano('0.1');
-        const createAffiliateResult = await affiliateMarketplaceContract.send(
-            bot.getSender(),
-            {
-                value: toNano('0.15'),
-            },
-            {
-                $$type: 'CreateAffiliate',
-                advertiser: advertiser.address,
-                publisher: publisher.address,
-                cpc: cpc,
-            }
-        );
-
-        for (const external of createAffiliateResult.externals) {
-            if (external.body) {
-                const decodedAffiliate = loadAffiliateCreatedEvent(external.body);
-                affiliateContractAddress = decodedAffiliate.affiliateContractAddress;
-            }
-        }
-
-        affiliateContract = blockchain.openContract(await Affiliate.fromAddress(Address.parse(affiliateContractAddress)));
     });
 
-    it('should fail if an unauthorized party tries to sign the contract as an advertiser', async () => {
-        const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
-        const unauthorizedPartyResult = await affiliateContract.send(
-            unauthorizedParty.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'AdvertiserSigned',
-            }
-        );
-
-        expect(unauthorizedPartyResult.transactions).toHaveTransaction({
-            from: unauthorizedParty.address,
-            to: affiliateContract.address,
-            success: false
-        });
-    });
-
-    it('should fail if advertiser tries to sign the contract twice', async () => {
-        // Advertiser signs the contract
-        await affiliateContract.send(
-            advertiser.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'AdvertiserSigned',
-            }
-        );
-
-        // Attempt to sign the contract again as the advertiser
-        const advertiserSignedAgainResult = await affiliateContract.send(
-            advertiser.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'AdvertiserSigned',
-            }
-        );
-
-        expect(advertiserSignedAgainResult.transactions).toHaveTransaction({
-            from: advertiser.address,
-            to: affiliateContract.address,
-            success: false
-        });
-    });
-
-    it('should fail if publisher tries to sign the contract twice', async () => {
-        // Publisher signs the contract
-        await affiliateContract.send(
-            publisher.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'PublisherSigned',
-            }
-        );
-
-        // Attempt to sign the contract again as the publisher
-        const publisherSignedAgainResult = await affiliateContract.send(
-            publisher.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'PublisherSigned',
-            }
-        );
-
-        expect(publisherSignedAgainResult.transactions).toHaveTransaction({
-            from: publisher.address,
-            to: affiliateContract.address,
-            success: false
-        });
-    });
-
-    it('should fail if advertiser tries to sign before affiliate creation', async () => {
-        const unauthorizedAdvertiser = await blockchain.treasury('unauthorizedAdvertiser');
-        const newAffiliateContract = blockchain.openContract(await Affiliate.fromInit(affiliateMarketplaceContract.address, 999, unauthorizedAdvertiser.address, publisher.address, toNano('0.1')));
-
-        const advertiserSignedWithoutCreationResult = await newAffiliateContract.send(
-            unauthorizedAdvertiser.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'AdvertiserSigned',
-            }
-        );
-
-        expect(advertiserSignedWithoutCreationResult.transactions).toHaveTransaction({
-            from: unauthorizedAdvertiser.address,
-            to: newAffiliateContract.address,
-            success: false
-        });
-    });
-
-    it('should fail if funds are added by an unauthorized party', async () => {
-        const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
-        const unauthorizedAddFundsResult = await affiliateContract.send(
-            unauthorizedParty.getSender(),
-            {
-                value: toNano('2'),
-            },
-            {
-                $$type: 'AddFunds',
-            }
-        );
-
-        expect(unauthorizedAddFundsResult.transactions).toHaveTransaction({
-            from: unauthorizedParty.address,
-            to: affiliateContract.address,
-            success: false
-        });
-    });
-
-    it('should fail if an unauthorized party tries to remove the affiliate', async () => {
-        const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
-        const unauthorizedRemoveAffiliateResult = await affiliateContract.send(
-            unauthorizedParty.getSender(),
-            {
-                value: toNano('0.02'),
-            },
-            {
-                $$type: 'RemoveAffiliateAndWithdrawFunds',
-            }
-        );
-
-        expect(unauthorizedRemoveAffiliateResult.transactions).toHaveTransaction({
-            from: unauthorizedParty.address,
-            to: affiliateContract.address,
-            success: false
-        });
-    });
-
-    it('should fail if someone other than the bot tries to create an affiliate', async () => {
+    // Negative Test Case 1: Only Advertiser Can Deploy New Affiliate
+    it('should fail if a non-advertiser tries to create an affiliate', async () => {
         const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
         const createAffiliateResult = await affiliateMarketplaceContract.send(
             unauthorizedParty.getSender(),
@@ -215,6 +53,7 @@ describe('AffiliateMarketplace Negative Tests', () => {
                 advertiser: advertiser.address,
                 publisher: publisher.address,
                 cpc: toNano('0.1'),
+                amount: toNano('10')
             }
         );
 
@@ -225,23 +64,260 @@ describe('AffiliateMarketplace Negative Tests', () => {
         });
     });
 
-    it('should fail if someone other than the owner tries to withdraw funds from the marketplace', async () => {
+    // Negative Test Case 2: Publisher Must Be Different From Advertiser
+    it('should fail if advertiser tries to create an affiliate with themselves as the publisher', async () => {
+        const createAffiliateResult = await affiliateMarketplaceContract.send(
+            advertiser.getSender(),
+            {
+                value: toNano('0.15'),
+            },
+            {
+                $$type: 'CreateAffiliate',
+                advertiser: advertiser.address,
+                publisher: advertiser.address,
+                cpc: toNano('0.1'),
+                amount: toNano('10')
+            }
+        );
+
+        expect(createAffiliateResult.transactions).toHaveTransaction({
+            from: advertiser.address,
+            to: affiliateMarketplaceContract.address,
+            success: false
+        });
+    });
+
+    // Negative Test Case 3: Publisher Cannot Be Zero Address
+    it('should fail if the publisher address is zero', async () => {
+        const zeroAddress = Address.parse('0:0000000000000000000000000000000000000000000000000000000000000000');
+        const createAffiliateResult = await affiliateMarketplaceContract.send(
+            advertiser.getSender(),
+            {
+                value: toNano('0.15'),
+            },
+            {
+                $$type: 'CreateAffiliate',
+                advertiser: advertiser.address,
+                publisher: zeroAddress,
+                cpc: toNano('0.1'),
+                amount: toNano('10')
+            }
+        );
+
+        expect(createAffiliateResult.transactions).toHaveTransaction({
+            from: advertiser.address,
+            to: affiliateMarketplaceContract.address,
+            success: false
+        });
+    });
+
+    // Negative Test Case 4: Insufficient Funds for Transaction Fee
+    it('should fail if there are insufficient funds for the transaction fee', async () => {
+        const createAffiliateResult = await affiliateMarketplaceContract.send(
+            advertiser.getSender(),
+            {
+                value: toNano('0.001'), 
+            },
+            {
+                $$type: 'CreateAffiliate',
+                advertiser: advertiser.address,
+                publisher: publisher.address,
+                cpc: toNano('0.1'),
+                amount: toNano('10')
+            }
+        );
+
+        expect(createAffiliateResult.transactions).toHaveTransaction({
+            from: advertiser.address,
+            to: affiliateMarketplaceContract.address,
+            success: false
+        });
+    });
+
+    // Negative Test Case 5: CPC Below Minimum Value
+    it('should fail if CPC is below the minimum value', async () => {
+        const minCpc = toNano('0.008'); // Assuming this is the minimum CPC value
+        const createAffiliateResult = await affiliateMarketplaceContract.send(
+            advertiser.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'CreateAffiliate',
+                advertiser: advertiser.address,
+                publisher: publisher.address,
+                cpc: minCpc - toNano('0.001'),
+                amount: toNano('10')
+            }
+        );
+
+        expect(createAffiliateResult.transactions).toHaveTransaction({
+            from: advertiser.address,
+            to: affiliateMarketplaceContract.address,
+            success: false
+        });
+    });
+
+    // Negative Test Case 6: Only Publisher Can Sign
+    it('should fail if a non-publisher tries to sign the contract', async () => {
+        
+        const createAffiliateResult = await affiliateMarketplaceContract.send(
+            advertiser.getSender(),
+            {
+                value: toNano('0.05'), 
+            },
+            {
+                $$type: 'CreateAffiliate',
+                advertiser: advertiser.address,
+                publisher: publisher.address,
+                cpc: toNano('0.1'),
+                amount: toNano('10')
+            }
+        );
+
+        expect(createAffiliateResult.transactions).toHaveTransaction({
+            from: advertiser.address,
+            to: affiliateMarketplaceContract.address,
+            success: false
+        });
+
+        const affiliateContract = blockchain.openContract(await Affiliate.fromInit(affiliateMarketplaceContract.address, 0, publisher.address, toNano('0.1')));
+        
         const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
-        const withdrawResult = await affiliateMarketplaceContract.send(
+        const publisherSignResult = await affiliateContract.send(
             unauthorizedParty.getSender(),
             {
                 value: toNano('0.01'),
             },
             {
-                $$type: 'AdminWithdraw',
-                amount: toNano('1'),
+                $$type: 'PublisherSigned',
             }
         );
 
-        expect(withdrawResult.transactions).toHaveTransaction({
+        expect(publisherSignResult.transactions).toHaveTransaction({
             from: unauthorizedParty.address,
-            to: affiliateMarketplaceContract.address,
+            to: affiliateContract.address,
             success: false
         });
     });
+
+    // // Negative Test Case 8: Only Parent Can Invoke InternalUserClick
+    // it('should fail if a non-parent address calls InternalUserClick', async () => {
+    //     const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
+    //     const userClickResult = await affiliateContract.send(
+    //         unauthorizedParty.getSender(),
+    //         {
+    //             value: toNano('0.01'),
+    //         },
+    //         {
+    //             $$type: 'InternalUserClick',
+    //         }
+    //     );
+
+    //     expect(userClickResult.transactions).toHaveTransaction({
+    //         from: unauthorizedParty.address,
+    //         to: affiliateContract.address,
+    //         success: false
+    //     });
+    // });
+
+    // // Negative Test Case 9: Incorrect State for User Click
+    // it('should fail if InternalUserClick is called in the wrong state', async () => {
+    //     const userClickResult = await affiliateContract.send(
+    //         affiliateMarketplaceContract.getSender(),
+    //         {
+    //             value: toNano('0.01'),
+    //         },
+    //         {
+    //             $$type: 'InternalUserClick',
+    //         }
+    //     );
+
+    //     expect(userClickResult.transactions).toHaveTransaction({
+    //         from: affiliateMarketplaceContract.address,
+    //         to: affiliateContract.address,
+    //         success: false
+    //     });
+    // });
+
+    // // Negative Test Case 10: Insufficient Contract Balance
+    // it('should fail if there is insufficient contract balance for InternalUserClick', async () => {
+    //     // Set up the state where the contract balance is insufficient
+    //     const userClickResult = await affiliateContract.send(
+    //         affiliateMarketplaceContract.getSender(),
+    //         {
+    //             value: toNano('0.01'),
+    //         },
+    //         {
+    //             $$type: 'InternalUserClick',
+    //         }
+    //     );
+
+    //     expect(userClickResult.transactions).toHaveTransaction({
+    //         from: affiliateMarketplaceContract.address,
+    //         to: affiliateContract.address,
+    //         success: false
+    //     });
+    // });
+
+    // // Negative Test Case 11: Only Owner Can Withdraw
+    // it('should fail if a non-owner tries to call AdminWithdraw', async () => {
+    //     const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
+    //     const withdrawResult = await affiliateMarketplaceContract.send(
+    //         unauthorizedParty.getSender(),
+    //         {
+    //             value: toNano('0.01'),
+    //         },
+    //         {
+    //             $$type: 'AdminWithdraw',
+    //             amount: toNano('1'),
+    //         }
+    //     );
+
+    //     expect(withdrawResult.transactions).toHaveTransaction({
+    //         from: unauthorizedParty.address,
+    //         to: affiliateMarketplaceContract.address,
+    //         success: false
+    //     });
+    // });
+
+    // // Negative Test Case 12: Insufficient Funds for Withdrawal
+    // it('should fail if there are insufficient funds for withdrawal', async () => {
+    //     const withdrawResult = await affiliateMarketplaceContract.send(
+    //         bot.getSender(),
+    //         {
+    //             value: toNano('0.01'),
+    //         },
+    //         {
+    //             $$type: 'AdminWithdraw',
+    //             amount: toNano('100'), // Assuming this amount is greater than the contract's balance
+    //         }
+    //     );
+
+    //     expect(withdrawResult.transactions).toHaveTransaction({
+    //         from: bot.address,
+    //         to: affiliateMarketplaceContract.address,
+    //         success: false
+    //     });
+    // });
+
+    // // Negative Test Case 13: Only Bot Can Invoke UserClick
+    // it('should fail if a non-bot address tries to call UserClick', async () => {
+    //     const unauthorizedParty = await blockchain.treasury('unauthorizedParty');
+    //     const userClickResult = await affiliateMarketplaceContract.send(
+    //         unauthorizedParty.getSender(),
+    //         {
+    //             value: toNano('0.01'),
+    //         },
+    //         {
+    //             $$type: 'UserClick',
+    //         }
+    //     );
+
+    //     expect(userClickResult.transactions).toHaveTransaction({
+    //         from: unauthorizedParty.address,
+    //         to: affiliateMarketplaceContract.address,
+    //         success: false
+    //     });
+    // });
 });
