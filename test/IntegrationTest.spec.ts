9668dbf085f26ac2c754a95ee2eeaf26f2cb0409
guyt-ds <58878443+guyt-ds@users.noreply.github.com>
2024-08-29 18:12:38 +0300

    Update IntegrationTest.spec.ts

--- test/IntegrationTest.spec.ts
+++ test/IntegrationTest.spec.ts
@@ -4,12 +4,6 @@ import { AffiliateMarketplace } from '../dist/tact_AffiliateMarketplace';
 import { Affiliate } from '../dist/tact_Affiliate';
 import '@ton/test-utils';
 
-// Import events and event loaders
-import {
-    loadAffiliateCreatedEvent,
-    loadPublisherSignedEvent,
-    loadPublisherPaidEvent,
-} from './events';
 
 type EmitLogEvent = {
     type: string;
@@ -17,6 +11,28 @@ type EmitLogEvent = {
 };
 
 const logs: EmitLogEvent[] = [];
+
+const EVENT_TYPE_AFFILIATE_CREATED = 3933896749; // AffiliateCreatedEvent
+
+
+function loadEvent(cell: Cell, expectedEventType: number) {
+    const slice = cell.beginParse();
+    const eventType = slice.loadUint(32);
+    if (eventType !== expectedEventType) {
+        console.log("EventType: " + eventType);
+        throw new Error("Unexpected event type");
+    }
+    return slice;
+}
+
+function loadAffiliateCreatedEvent(cell: Cell) {
+    const slice = loadEvent(cell, EVENT_TYPE_AFFILIATE_CREATED);
+    const affiliateId = slice.loadUint(32);
+    const advertiser = slice.loadAddress().toString();
+    const publisher = slice.loadAddress().toString();
+    const affiliateContractAddress = slice.loadAddress();
+    return { $$type: 'AffiliateCreatedEvent', affiliateId, advertiser, publisher, affiliateContractAddress };
+}
 
 describe('AffiliateMarketplace Integration Test', () => {
     let blockchain;
@@ -53,7 +69,6 @@ describe('AffiliateMarketplace Integration Test', () => {
     it('should create affiliate,sign by publisher, and handle user click', async () => {
         
         const cpc = toNano('0.1');
-        const amount = toNano('10');
 
         logs.push({ type: 'AffiliateMarketplaceContractBalance', data: fromNano(await affiliateMarketplaceContract.getBalance()) + " TON"  });
         logs.push({ type: 'AdvertiserBalance', data: fromNano(await advertiser.getBalance()) + " TON"  });
@@ -61,21 +76,19 @@ describe('AffiliateMarketplace Integration Test', () => {
 
         // Create Affiliate
         const createAffiliateResult = await affiliateMarketplaceContract.send(
-            advertiser.getSender(),
+            bot.getSender(),
             {
-                value: amount, // load contract with 10 TON
+                value: toNano('10'), // load contract with 10 TON + fees
             },
             {
                 $$type: 'CreateAffiliate',
                 advertiser: advertiser.address,
-                publisher: publisher.address,
-                cpc: cpc,
-                amount: amount
+                publisher: publisher.address
             }
         );
 
         expect(createAffiliateResult.transactions).toHaveTransaction({
-            from: advertiser.address,
+            from: bot.address,
             to: affiliateMarketplaceContract.address,
             success: true,
         });
@@ -89,8 +102,7 @@ describe('AffiliateMarketplace Integration Test', () => {
                     $$type: 'AffiliateCreatedEvent',
                     affiliateId: 0,
                     advertiser: advertiser.address.toString(),
-                    publisher: publisher.address.toString(),
-                    cpc: cpc
+                    publisher: publisher.address.toString()
                 });
                 affiliateContractAddress = decodedAffiliate.affiliateContractAddress;
             }
@@ -104,87 +116,87 @@ describe('AffiliateMarketplace Integration Test', () => {
             deploy: true,
         });
 
-        const affiliateContract = blockchain.openContract(await Affiliate.fromInit(affiliateMarketplaceContract.address, 0, publisher.address, cpc));
+     
+        // const affiliateContract = blockchain.openContract(await Affiliate.fromAddress(affiliateContractAddress));
 
-
-        logs.push({ type: 'AffiliateMarketplaceContractBalance', data: fromNano(await affiliateMarketplaceContract.getBalance()) + " TON"  });
-        logs.push({ type: 'AffiliateContractBalance', data: fromNano(await affiliateContract.getBalance()) + " TON" });
-        logs.push({ type: 'AdvertiserBalance', data: fromNano(await advertiser.getBalance()) + " TON"  });
-        logs.push({ type: 'PublisherBalance', data: fromNano(await publisher.getBalance()) + " TON"  });
+        // logs.push({ type: 'AffiliateMarketplaceContractBalance', data: fromNano(await affiliateMarketplaceContract.getBalance()) + " TON"  });
+        // logs.push({ type: 'AffiliateContractBalance', data: fromNano(await affiliateContract.getBalance()) + " TON" });
+        // logs.push({ type: 'AdvertiserBalance', data: fromNano(await advertiser.getBalance()) + " TON"  });
+        // logs.push({ type: 'PublisherBalance', data: fromNano(await publisher.getBalance()) + " TON"  });
 
         
-        // Publisher signs the contract
-        const publisherSignedResult = await affiliateContract.send(
-            publisher.getSender(),
-            {
-                value: toNano('0.01'),
-            },
-            {
-                $$type: 'PublisherSigned',
-            }
-        );
+        // // Publisher signs the contract
+        // const publisherSignedResult = await affiliateContract.send(
+        //     publisher.getSender(),
+        //     {
+        //         value: toNano('0.01'),
+        //     },
+        //     {
+        //         $$type: 'PublisherSigned',
+        //     }
+        // );
 
-        expect(publisherSignedResult.transactions).toHaveTransaction({
-            from: publisher.address,
-            to: affiliateContract.address,
-            success: true,
-        });
+        // expect(publisherSignedResult.transactions).toHaveTransaction({
+        //     from: publisher.address,
+        //     to: affiliateContract.address,
+        //     success: true,
+        // });
 
-        for (const external of publisherSignedResult.externals) {
-            if (external.body) {
-                const decodedEvent = loadPublisherSignedEvent(external.body);
-                logs.push({ type: 'PublisherSignedEvent', data: decodedEvent });
-                expect(decodedEvent).toMatchObject({
-                    $$type: 'PublisherSignedEvent',
-                    affiliateId: 0,
-                    publisher: publisher.address.toString(),
-                    cpc: cpc,
-                });
-            }
-        }
+        // for (const external of publisherSignedResult.externals) {
+        //     if (external.body) {
+        //         const decodedEvent = loadPublisherSignedEvent(external.body);
+        //         logs.push({ type: 'PublisherSignedEvent', data: decodedEvent });
+        //         expect(decodedEvent).toMatchObject({
+        //             $$type: 'PublisherSignedEvent',
+        //             affiliateId: 0,
+        //             publisher: publisher.address.toString(),
+        //             cpc: cpc,
+        //         });
+        //     }
+        // }
 
-        // Handle User Click
-        const userClickResult = await affiliateMarketplaceContract.send(
-            bot.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'UserClick',
-                affiliateId: 0,
-                advertiser: advertiser.address,
-                publisher: publisher.address,
-                cpc: cpc,
-            }
-        );
+        // // Handle User Click
+        // const userClickResult = await affiliateMarketplaceContract.send(
+        //     bot.getSender(),
+        //     {
+        //         value: toNano('0.05'),
+        //     },
+        //     {
+        //         $$type: 'UserClick',
+        //         affiliateId: 0,
+        //         advertiser: advertiser.address,
+        //         publisher: publisher.address,
+        //         cpc: cpc,
+        //     }
+        // );
 
-        expect(userClickResult.transactions).toHaveTransaction({
-            from: affiliateMarketplaceContract.address,
-            to: affiliateContract.address,
-            success: true,
-        });
+        // expect(userClickResult.transactions).toHaveTransaction({
+        //     from: affiliateMarketplaceContract.address,
+        //     to: affiliateContract.address,
+        //     success: true,
+        // });
 
-        for (const external of userClickResult.externals) {
-            if (external.body) {
-                const decodedEvent = loadPublisherPaidEvent(external.body);
-                logs.push({ type: 'PublisherPaidEvent', data: decodedEvent });
-                expect(decodedEvent).toMatchObject({
-                    $$type: 'PublisherPaidEvent',
-                    affiliateId: 0,
-                    publisher: publisher.address.toString(),
-                    cpc: cpc,
-                });
-            }
-        }
+        // for (const external of userClickResult.externals) {
+        //     if (external.body) {
+        //         const decodedEvent = loadPublisherPaidEvent(external.body);
+        //         logs.push({ type: 'PublisherPaidEvent', data: decodedEvent });
+        //         expect(decodedEvent).toMatchObject({
+        //             $$type: 'PublisherPaidEvent',
+        //             affiliateId: 0,
+        //             publisher: publisher.address.toString(),
+        //             cpc: cpc,
+        //         });
+        //     }
+        // }
 
-        // Check that the click was registered and CPC was paid
-        const numUserClicks = await affiliateContract.getNumUserClicks();
-        expect(numUserClicks).toBe(1n);
+        // // Check that the click was registered and CPC was paid
+        // const numUserClicks = await affiliateContract.getNumUserClicks();
+        // expect(numUserClicks).toBe(1n);
 
-        logs.push({ type: 'AffiliateMarketplaceContractBalance', data: fromNano(await affiliateMarketplaceContract.getBalance()) + " TON"  });
-        logs.push({ type: 'AffiliateContractBalance', data: fromNano(await affiliateContract.getBalance()) + " TON" });
-        logs.push({ type: 'AdvertiserBalance', data: fromNano(await advertiser.getBalance()) + " TON"  });
-        logs.push({ type: 'PublisherBalance', data: fromNano(await publisher.getBalance()) + " TON"  });
+        // logs.push({ type: 'AffiliateMarketplaceContractBalance', data: fromNano(await affiliateMarketplaceContract.getBalance()) + " TON"  });
+        // logs.push({ type: 'AffiliateContractBalance', data: fromNano(await affiliateContract.getBalance()) + " TON" });
+        // logs.push({ type: 'AdvertiserBalance', data: fromNano(await advertiser.getBalance()) + " TON"  });
+        // logs.push({ type: 'PublisherBalance', data: fromNano(await publisher.getBalance()) + " TON"  });
 
 
 
