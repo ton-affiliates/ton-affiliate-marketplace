9a0b58f9fe6096f33ef04e6d33aac57c91b71531
guyt-ds <58878443+guyt-ds@users.noreply.github.com>
2024-09-20 19:18:25 +0300

    Update events.ts

--- test/events.ts
+++ test/events.ts
@@ -7,6 +7,8 @@ const EVENT_TYPE_CAMPAIGN_CREATED = 2452245169;
 const EVENT_TYPE_AFFILIATE_CREATED = 627662741; 
 const EVENT_TYPE_AFFILIATE_WITHDRAW_EARNINGS = 1950324544; 
 const EVENT_TYPE_ADVERTISER_REPLENISH = 3518853408;
+const EVENT_TYPE_CAMPAIGN_BALANCE_UNDER_THRESHOLD = 859219313;
+const EVENT_TYPE_CAMPAIGN_REMOVED = 88274163;
 
 function loadEvent(cell: Cell, expectedEventType: number) {
     const slice = cell.beginParse();
@@ -16,6 +18,24 @@ function loadEvent(cell: Cell, expectedEventType: number) {
     }
     return slice;
 }
+
+export function loadCampaignUnderThresholdEvent(cell: Cell) {
+    const slice = loadEvent(cell, EVENT_TYPE_CAMPAIGN_BALANCE_UNDER_THRESHOLD);
+    const campaignId = slice.loadUint(32);
+    const advertiserAddressStr = slice.loadAddress().toString();
+    const campaginBalance = slice.loadCoins();
+    return { $$type: 'CampaignBalnceUnderThresholdEvent', campaignId, advertiserAddressStr, campaginBalance };
+}
+
+export function loadCampaignRemovedEvent(cell: Cell) {
+    const slice = loadEvent(cell, EVENT_TYPE_CAMPAIGN_REMOVED);
+    const campaignId = slice.loadUint(32);
+    const advertiserAddressStr = slice.loadAddress().toString();
+    const campaginBalance = slice.loadCoins();
+    const contractBalance = slice.loadCoins();
+    return { $$type: 'CampaignRemovedEvent', campaignId, advertiserAddressStr, campaginBalance, contractBalance };
+}
+
 
 export function loadCampaignCreatedEvent(cell: Cell) {
     const slice = loadEvent(cell, EVENT_TYPE_CAMPAIGN_CREATED);
@@ -49,3 +69,4 @@ export function  loadAdvertiserReplenisEvent(cell: Cell) {
     const replenishAmount = slice.loadCoins();
     return { $$type: 'AdvertiserReplenisEvent', campaignId, advertiserAddressStr, replenishAmount };
 }
+
