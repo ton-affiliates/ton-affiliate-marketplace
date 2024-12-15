770c897554b4caf1ecec3871b8e7f09481be1f51
guyCCT <guy@cryptocopytraders.com>
2024-12-15 16:06:44 +0200

    add new script

---
+++ scripts/advertiser/signOffAllAffiliates.ts
@@ -0,0 +1,58 @@
+import { toNano, Address, fromNano, Dictionary } from '@ton/core';
+import { Campaign } from '../../wrappers/Campaign';
+import { AffiliateMarketplace } from '../../wrappers/AffiliateMarketplace';
+import { NetworkProvider, sleep } from '@ton/blueprint';
+import { AFFILIATE_MARKETPLACE_ADDRESS, MAX_ATTEMPTS, GAS_FEE } from '../constants'
+
+export async function run(provider: NetworkProvider, args: string[]) {
+    
+	const ui = provider.ui();
+
+    const affiliateMarketplace = provider.open(await AffiliateMarketplace.fromAddress(AFFILIATE_MARKETPLACE_ADDRESS));
+    const campaignId = BigInt(args.length > 0 ? args[0] : await ui.input('Campaign id'));
+	const advertiser = Address.parse(args.length > 1 ? args[1] : await ui.input('Advertiser address'));	
+	
+	let campaignAddress = await affiliateMarketplace.getCampaignContractAddress(campaignId, advertiser);
+	if (!(await provider.isContractDeployed(campaignAddress))) {
+        ui.write(`Error: Contract at address ${campaignAddress} is not deployed!`);
+        return;
+    }
+	
+	const campaign = provider.open(Campaign.fromAddress(campaignAddress));
+	let numAdvertiserSignOffsBefore = (await campaign.getCampaignData()).numAdvertiserSignOffs;
+
+			
+	await campaign.send(
+        provider.sender(),
+        { 
+			value: toNano('0.3')  // remainder will be returned to advertiser 
+		},
+        { 
+			$$type: 'AdvertiserSignOffAllAffilliates'
+		}
+	);
+		
+    ui.write('Waiting for campaign to update numAdvertiserSignOffs...');
+	
+	let numAdvertiserSignOffsAfter = (await campaign.getCampaignData()).numAdvertiserSignOffs;
+    let attempt = 1;
+	
+    while (numAdvertiserSignOffsBefore == numAdvertiserSignOffsAfter) {
+		
+		if (attempt == MAX_ATTEMPTS) {
+			// tx failed
+			ui.write(`Error: TX failed or timedout!`);
+			return;
+		}
+	
+        ui.setActionPrompt(`Attempt ${attempt}`);
+        await sleep(2000);
+		numAdvertiserSignOffsAfter = (await campaign.getCampaignData()).numAdvertiserSignOffs;
+        attempt++;
+    }
+	
+    ui.clearActionPrompt();
+    ui.write('Adveriser signed off all affiliates successfully!');
+
+	
+}
